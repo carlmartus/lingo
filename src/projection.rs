3a5ae27262e2cfe1840360363c58219ff224c207
Martin Sandgren <carlmartus@gmail.com>
2018-04-14 23:54:47 +0200

    Orthogonal projection

--- src/projection.rs
+++ src/projection.rs
@@ -24,4 +24,21 @@ impl Matrix4x4 {
             self.values[*i] = 0f32
         });
     }
+
+    pub fn ortho(&mut self, x0: f32, y0: f32, x1: f32, y1: f32) {
+
+        self.values[ 0] = 2f32 / (x1-x0);
+        self.values[ 5] = 2f32 / (y1-y0);
+        self.values[10] = 1f32;
+        self.values[15] = 1f32;
+
+        self.values[12] = -(x1+x0)/(x1-x0);
+        self.values[13] = -(y1+y0)/(y1-y0);
+        self.values[14] = 0f32;
+
+        // Set rest to 0
+        [1, 2, 3, 4, 6, 7, 8, 9].iter().for_each(|i| {
+            self.values[*i] = 0f32;
+        });
+    }
 }
