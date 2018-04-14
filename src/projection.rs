4d12db364e1dfa9cc3026f06ddd497aa2781630f
Martin Sandgren <carlmartus@gmail.com>
2018-04-14 23:55:24 +0200

    Perspective projection

--- src/projection.rs
+++ src/projection.rs
@@ -41,4 +41,25 @@ impl Matrix4x4 {
             self.values[*i] = 0f32;
         });
     }
+
+    pub fn perspective(&mut self, fov: f32, ratio: f32, near: f32, far: f32) {
+        let size = near * (fov * 0.5).tan();
+        let left = -size;
+        let right = size;
+        let bottom = -size / ratio;
+        let top = size / ratio;
+
+        self.values[0] = 2f32 * near / (right - left);
+        self.values[5] = 2f32 * near / (top - bottom);
+        self.values[8] = (right + left) / (right - left);
+        self.values[9] = (top + bottom) / (top - bottom);
+        self.values[10] = -(far + near) / (far - near);
+        self.values[11] = -1f32;
+        self.values[14] = -(2f32 * far * near) / (far - near);
+
+        // Set rest to 0
+        [1, 2, 3, 4, 6, 7, 12, 13, 15].iter().for_each(|i| {
+            self.values[*i] = 0f32;
+        });
+    }
 }
